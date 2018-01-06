ddbd1813ac996ee6d7e084fa4701763ffceeeeee
battle <1074849188@qq.com>
2018-01-06 14:47:30 +0800

    第一次提交java project 工程demo

---
+++ demo/src/com/it/demo/demo.java
@@ -0,0 +1,12 @@
+package com.it.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class demo {
+	@Test
+	public void testName() throws Exception {
+		System.out.println("hello world");
+	}
+}
